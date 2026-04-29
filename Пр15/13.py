25a59e255d373c15c6551ab960c20e9fcc5f6e22
beetlisa <kasikandriera696@yandex.ru>
2026-04-29 12:52:29 +0500

    Update 13.py

--- Пр15/13.py
+++ Пр15/13.py
@@ -2,8 +2,8 @@ from datetime import timedelta
 from datetime import date
 
 given_date = date(2024, 12, 31)
-days_ahead = 0 - given_date.weekday() # 0 = понедельник
+days_ahead = 0 - given_date.weekday()
 if days_ahead <= 0:
     days_ahead += 7
 next_monday = given_date + timedelta(days=days_ahead)
-print(f"Ближайший понедельник: {next_monday}")
+print(f"Next Monday: {next_monday}")
